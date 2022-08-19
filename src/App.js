c69f87853720cd22a47b9410370869ec8b3a2d29
SooBin An <jejupalette@naver.com>
2022-08-19 13:56:29 +0900

    feat : App에서 todos 상태 사용

--- src/App.js
+++ src/App.js
@@ -2,12 +2,30 @@ import React from 'react'
 import TodoTemplate from './components/TodoTemplate'
 import TodoInsert from './components/TodoInsert'
 import TodoList from './components/TodoList'
+import { useState } from 'react'
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '리엑트 기초 공부하기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '컴포넌트 스타일링 해보기',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '일정 관리 앱 만들어 보기',
+      checked: true,
+    },
+  ])
   return (
     <TodoTemplate>
       <TodoInsert />
-      <TodoList />
+      <TodoList todos={todos} />
     </TodoTemplate>
   )
 }
--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -1,11 +1,16 @@
 import styles from './TodoListItem.module.css'
 
-const TodoListItem = () => {
+const TodoListItem = ({ todo }) => {
+  const { text, checked } = todo
   return (
     <div className={styles.TodoListItem}>
-      <div className={styles.checkbox}>
-        <i class="bi bi-emoji-neutral"></i>
-        <div className={styles.text}>Todo</div>
+      <div className={`${styles.checkbox} ${checked && styles.checked}`}>
+        {checked ? (
+          <i class="bi bi-emoji-smile-fill"></i>
+        ) : (
+          <i class="bi bi-emoji-neutral"></i>
+        )}
+        <div className={styles.text}>{text}</div>
       </div>
       <div className={styles.remove}>
         <i class="bi bi-x-circle-fill"></i>
